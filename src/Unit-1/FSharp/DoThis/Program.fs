100d99c1e39f6fa58d971e52f511e16553f11ad1
John S Azariah <john.azariah@gmail.com>
2015-02-26 02:23:36 +1100

    Added TO-DO solution for F#

---
+++ src/Unit-1/FSharp/DoThis/Program.fs
@@ -0,0 +1,33 @@
+﻿open System
+open Akka.FSharp
+open Akka.FSharp.Spawn
+open Akka.Actor
+
+let printInstructions () =
+    Console.WriteLine "Write whatever you want into the console!"
+    Console.Write "Some lines will appear as"
+    Console.ForegroundColor <- ConsoleColor.DarkRed
+    Console.Write " red "
+    Console.ResetColor ()
+    Console.Write " and others will appear as"
+    Console.ForegroundColor <- ConsoleColor.Green
+    Console.Write " green! "
+    Console.ResetColor ()
+    Console.WriteLine ()
+    Console.WriteLine ()
+    Console.WriteLine "Type 'exit' to quit this application at any time.\n"
+
+[<EntryPoint>]
+let main argv = 
+    // initialize an actor system
+    // YOU NEED TO FILL IN HERE
+    printInstructions ()
+    
+    // make your first actors using the 'spawn' function
+    // YOU NEED TO FILL IN HERE
+
+    // tell the consoleReader actor to begin
+    // YOU NEED TO FILL IN HERE
+
+    myActorSystem.AwaitTermination ()
+    0
