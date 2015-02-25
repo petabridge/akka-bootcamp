94b69c6928c9d5acfec36c1965ae30623921a92b
Jorge Fioranelli <jorge.fioranelli@gmail.com>
2015-02-25 12:14:15 +1100

    Added F# solutions for lessons 1.1, 1.2, 1.3 and 1.4

---
+++ src/Unit-1/FSharp/lesson4/Completed/Program.fs
@@ -0,0 +1,14 @@
+﻿open System
+open Akka.FSharp
+open Akka.Actor
+
+[<EntryPoint>]
+let main argv = 
+    let myActorSystem = System.create "MyActorSystem" (Configuration.load ())
+    let consoleWriterActor = spawnOpt myActorSystem "ConsoleWriterActor" (actorOf Actors.consoleWriterActor) [SpawnOption.SupervisorStrategy(SupervisorStrategy.DefaultStrategy)]
+    let tailCoordinatorActor = spawnOpt myActorSystem "TailCoordinatorActor" (actorOf2 Actors.tailCoordinatorActor) [SpawnOption.SupervisorStrategy(Strategies.tailCoordinatorStrategy ())]
+    let fileValidatorActor = spawnOpt myActorSystem "ValidationActor" (actorOf2 (Actors.fileValidatorActor consoleWriterActor tailCoordinatorActor)) [SpawnOption.Deploy(Deploy.Local)]
+    let consoleReaderActor = spawnOpt myActorSystem "ConsoleReaderActor" (actorOf2 (Actors.consoleReaderActor fileValidatorActor)) [SpawnOption.Router(Akka.Routing.RouterConfig.NoRouter)]
+    consoleReaderActor <! Actors.StartCommand
+    myActorSystem.AwaitTermination ()
+    0
---
+++ src/Unit-1/FSharp/lesson4/Completed/Strategies.fs
@@ -0,0 +1,12 @@
+﻿module Strategies
+
+open System
+open Akka.Actor
+open Akka.FSharp
+
+let tailCoordinatorStrategy () = 
+    Strategy.OneForOne((fun ex ->
+        match ex with 
+        | :? ArithmeticException  -> Directive.Resume
+        | :? NotSupportedException -> Directive.Stop
+        | _ -> Directive.Restart), 10, TimeSpan.FromSeconds(30.))
