618950fbb3aa8b1c5064491f8475c80c2400dc04
Jorge Fioranelli <jorge.fioranelli@gmail.com>
2015-02-25 12:14:15 +1100

    Added F# solutions for lessons 1.1, 1.2, 1.3 and 1.4

---
+++ src/Unit-1/FSharp/lesson1/Completed/Actors.fs
@@ -0,0 +1,29 @@
+﻿module Actors
+
+open System
+open Akka.Actor
+open Akka.FSharp
+
+[<Literal>]
+let ExitCommand = "exit"
+
+let consoleReaderActor (consoleWriter: ActorRef) (mailbox: Actor<_>) message = 
+    let read = Console.ReadLine ()
+    match read.ToLower () with
+    | ExitCommand -> mailbox.Context.System.Shutdown ()
+    | _ -> 
+        consoleWriter <! read
+        mailbox.Self <! "continue"
+
+let consoleWriterActor message = 
+    let (|Even|Odd|) n = if n % 2 = 0 then Even else Odd
+    
+    let printInColor color message =
+        Console.ForegroundColor <- color
+        Console.WriteLine (message.ToString ())
+        Console.ResetColor ()
+
+    match message.ToString().Length with
+    | 0    -> printInColor ConsoleColor.DarkYellow "Please provide an input.\n"
+    | Even -> printInColor ConsoleColor.Red "Your string had an even # of characters.\n"
+    | Odd  -> printInColor ConsoleColor.Green "Your string had an odd # of characters.\n"
