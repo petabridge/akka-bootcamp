d7d3e059dc1faa43739c3f8a4e23068c21e47910
Jorge Fioranelli <jorge.fioranelli@gmail.com>
2015-02-25 12:14:15 +1100

    Added F# solutions for lessons 1.1, 1.2, 1.3 and 1.4

---
+++ src/Unit-1/FSharp/lesson3/Completed/Messages.fs
@@ -0,0 +1,13 @@
+﻿[<AutoOpen>]
+module Messages
+
+type ProcessCommand = 
+| ContinueProcessing
+
+type ErrorType =
+| Null
+| Validation
+
+type InputResult =
+| InputSuccess of string
+| InputError of reason: string * errorType: ErrorType
