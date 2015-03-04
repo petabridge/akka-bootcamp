59cfd6f2d6025b4f2cfa12b20fdb35b6119b9c7c
Aaron Stannard <aaron@petabridge.com>
2015-03-03 17:28:03 -0800

    creating initial projet

---
+++ src/Unit-3/DoThis/RepoResultsForm.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace GithubActors
+{
+    partial class RepoResultsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Login = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.URL = new System.Windows.Forms.DataGridViewLinkColumn();
+            this.Repos = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Followers = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Following = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.tsProgress = new System.Windows.Forms.ToolStripProgressBar();
+            this.tsStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Login,
+            this.URL,
+            this.Repos,
+            this.Followers,
+            this.Following});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(739, 322);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Login
+            // 
+            this.Login.HeaderText = "Login";
+            this.Login.Name = "Login";
+            // 
+            // URL
+            // 
+            this.URL.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.URL.HeaderText = "URL";
+            this.URL.Name = "URL";
+            // 
+            // Repos
+            // 
+            this.Repos.HeaderText = "Repos";
+            this.Repos.Name = "Repos";
+            // 
+            // Followers
+            // 
+            this.Followers.HeaderText = "Followers";
+            this.Followers.Name = "Followers";
+            // 
+            // Following
+            // 
+            this.Following.HeaderText = "Following";
+            this.Following.Name = "Following";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsProgress,
+            this.tsStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 300);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(739, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // tsProgress
+            // 
+            this.tsProgress.Name = "tsProgress";
+            this.tsProgress.Size = new System.Drawing.Size(100, 16);
+            // 
+            // tsStatus
+            // 
+            this.tsStatus.Name = "tsStatus";
+            this.tsStatus.Size = new System.Drawing.Size(73, 17);
+            this.tsStatus.Text = "Processing...";
+            // 
+            // RepoResultsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(739, 322);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "RepoResultsForm";
+            this.Text = "Starrers for {RepoName}";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Login;
+        private System.Windows.Forms.DataGridViewLinkColumn URL;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Repos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Followers;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Following;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripProgressBar tsProgress;
+        private System.Windows.Forms.ToolStripStatusLabel tsStatus;
+    }
+}
