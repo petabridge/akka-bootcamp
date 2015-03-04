6b43fe78b8cfe0bc21afbdbe3d2d0c06fc77722b
Aaron Stannard <aaron@petabridge.com>
2015-03-03 17:28:03 -0800

    creating initial projet

---
+++ src/Unit-3/DoThis/MainForm.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace GithubActors
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblRepo = new System.Windows.Forms.Label();
+            this.lblIsValid = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(95, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(455, 24);
+            this.textBox1.TabIndex = 0;
+            // 
+            // lblRepo
+            // 
+            this.lblRepo.AutoSize = true;
+            this.lblRepo.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRepo.Location = new System.Drawing.Point(3, 16);
+            this.lblRepo.Name = "lblRepo";
+            this.lblRepo.Size = new System.Drawing.Size(86, 18);
+            this.lblRepo.TabIndex = 1;
+            this.lblRepo.Text = "Repo URL";
+            // 
+            // lblIsValid
+            // 
+            this.lblIsValid.AutoSize = true;
+            this.lblIsValid.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIsValid.Location = new System.Drawing.Point(95, 44);
+            this.lblIsValid.Name = "lblIsValid";
+            this.lblIsValid.Size = new System.Drawing.Size(46, 18);
+            this.lblIsValid.TabIndex = 2;
+            this.lblIsValid.Text = "label1";
+            this.lblIsValid.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(218, 90);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 37);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "GO";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(562, 151);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblIsValid);
+            this.Controls.Add(this.lblRepo);
+            this.Controls.Add(this.textBox1);
+            this.Name = "MainForm";
+            this.Text = "Who Starred This Repo?";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblRepo;
+        private System.Windows.Forms.Label lblIsValid;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
