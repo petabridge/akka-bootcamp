d73e6658a16d55d69f534f7a7cc0e9edc3fcaeb5
Jorge Fioranelli <jorge.fioranelli@gmail.com>
2015-03-09 20:19:10 +1100

    Improved F# config and form code for lessons 2.1 and 2.2

--- src/Unit-2/lesson1/FSharp/Completed/Program.fs
+++ src/Unit-2/lesson1/FSharp/Completed/Program.fs
@@ -12,41 +12,23 @@ open System.Configuration
 open Actors
 
 let section = ConfigurationManager.GetSection "akka" :?> AkkaConfigurationSection
-let config = section.AkkaConfig
-let chartActors = ActorSystem.Create ("ChartActors", config)
+let chartActors = System.create "ChartActors" section.AkkaConfig // TODO: use Configuration.load when this fix is released https://github.com/akkadotnet/akka.net/issues/671
 
 Application.EnableVisualStyles ()
 Application.SetCompatibleTextRenderingDefault false
 
-let seriesCounter = new AtomicCounter(1)
-let sysChart = new Chart()
-let form = new Form()
-form.Visible <- true 
-let chartArea1 = new ChartArea()
-let legend1 = new Legend()
-let series1 = new Series()
+let seriesCounter = AtomicCounter(1)
+let sysChart = new Chart(Name = "sysChart", Text = "sysChart", Dock = DockStyle.Fill, Location = Point(0, 0), Size = Size(684, 446), TabIndex = 0)
+let form = new Form(Name = "Main", Visible = true, Text = "System Metrics", AutoScaleDimensions = SizeF(6.F, 13.F), AutoScaleMode = AutoScaleMode.Font, ClientSize = Size(684, 446))
+let chartArea1 = new ChartArea(Name = "ChartArea1")
+let legend1 = new Legend(Name = "Legend1")
+let series1 = new Series(Name = "Series1", ChartArea = "ChartArea1", Legend = "Legend1")
 sysChart.BeginInit ()
 form.SuspendLayout ()
-chartArea1.Name <- "ChartArea1"
 sysChart.ChartAreas.Add chartArea1
-sysChart.Dock <- DockStyle.Fill
-legend1.Name <- "Legend1"
 sysChart.Legends.Add legend1
-sysChart.Location <- Point(0, 0)
-sysChart.Name <- "sysChart"
-series1.ChartArea <- "ChartArea1"
-series1.Legend <- "Legend1"
-series1.Name <- "Series1"
 sysChart.Series.Add series1
-sysChart.Size <- new Size(684, 446)
-sysChart.TabIndex <- 0;
-sysChart.Text <- "sysChart"
-form.AutoScaleDimensions <- new SizeF(6.F, 13.F)
-form.AutoScaleMode <- AutoScaleMode.Font
-form.ClientSize <- Size(684, 446)
 form.Controls.Add sysChart
-form.Name <- "Main"
-form.Text <- "System Metrics"
 sysChart.EndInit ()
 form.ResumeLayout false
 
