0725f24a93716aabf5f31966b26f254c81f29824
Aaron Stannard <aaron@petabridge.com>
2015-05-31 15:21:01 -0700

    close #126
    Fixed race condition when closing form in Unit 2. ActorSystem now gets
    terminated first so no more writes are attempted to the chart before
    it's removed from the render panel.

--- src/Unit-2/lesson4/Completed/Main.cs
+++ src/Unit-2/lesson4/Completed/Main.cs
@@ -48,6 +48,15 @@ namespace ChartApp
             _toggleActors[CounterType.Cpu].Tell(new ButtonToggleActor.Toggle());
         }
 
+        private void Main_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //shut down the charting actor
+            _chartActor.Tell(PoisonPill.Instance);
+
+            //shut down the ActorSystem
+            Program.ChartActors.Shutdown();
+        }
+
         #endregion
 
         #region Button handlers
