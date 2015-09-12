02273f76b6e3741566d05cb3085b1e273506b37e
Bobby <bobbychopra@gmail.com>
2015-09-12 13:48:50 -0400

    Conform Unit 1, lesson 1 to be more F#
    	modified:   src/Unit-1/FSharp/DoThis/Actors.fs
    	modified:   src/Unit-1/FSharp/lesson1/Completed/Actors.fs
    	modified:   src/Unit-1/FSharp/lesson1/README.md

--- src/Unit-1/FSharp/DoThis/Program.fs
+++ src/Unit-1/FSharp/DoThis/Program.fs
@@ -2,12 +2,13 @@
 open Akka.FSharp
 open Akka.FSharp.Spawn
 open Akka.Actor
+open WinTail
 
 let printInstructions () =
     Console.WriteLine "Write whatever you want into the console!"
     Console.Write "Some lines will appear as"
-    Console.ForegroundColor <- ConsoleColor.DarkRed
-    Console.Write " red "
+    Console.ForegroundColor <- ConsoleColor.Red
+    Console.Write " red"
     Console.ResetColor ()
     Console.Write " and others will appear as"
     Console.ForegroundColor <- ConsoleColor.Green
