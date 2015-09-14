ff16e0eb84f0e9bb27c00cb318ec061693bc9025
Tom Kearney <tomk@digiterre.com>
2015-09-14 19:39:09 +0100

    unit-1 lesson2

---
+++ src/Unit-1/DoThis/Messages.cs
@@ -0,0 +1,39 @@
+﻿namespace WinTail
+{
+    public class Messages
+    {
+        public class ContinueProcessing
+        {
+        }
+
+        public class InputSuccess
+        {
+            public InputSuccess(string reason)
+            {
+                Reason = reason;
+            }
+
+            public string Reason { get; private set; }
+        }
+
+        public class InputError
+        {
+            public InputError(string reason)
+            {
+                Reason = reason;
+            }
+
+            public string Reason { get; private set; }
+        }
+
+        public class NullInputError : InputError
+        {
+            public NullInputError(string reason) : base(reason) { }
+        }
+
+        public class ValidationError : InputError
+        {
+            public ValidationError(string reason) : base(reason) { }
+        }
+    }
+}
--- src/Unit-1/DoThis/Program.cs
+++ src/Unit-1/DoThis/Program.cs
@@ -12,33 +12,15 @@ namespace WinTail
         {
             MyActorSystem = ActorSystem.Create("MyActorSystem");
 
-            PrintInstructions();
-
             var consoleWriterActor = MyActorSystem.ActorOf(Props.Create(() => new ConsoleWriterActor()));
             var consoleReaderActor = MyActorSystem.ActorOf(Props.Create(() => new ConsoleReaderActor(consoleWriterActor)));
 
             // tell console reader to begin
-            consoleReaderActor.Tell(null);
-            consoleReaderActor.Tell("start");
+            consoleWriterActor.Tell(null);
+            consoleReaderActor.Tell(ConsoleReaderActor.StartCommand);
             
             // blocks the main thread from exiting until the actor system is shut down
             MyActorSystem.AwaitTermination();
-        }
-
-        private static void PrintInstructions()
-        {
-            Console.WriteLine("Write whatever you want into the console!");
-            Console.Write("Some lines will appear as");
-            Console.ForegroundColor = ConsoleColor.DarkRed;
-            Console.Write(" red ");
-            Console.ResetColor();
-            Console.Write(" and others will appear as");
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.Write(" green! ");
-            Console.ResetColor();
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine($"Type '{ConsoleReaderActor.ExitCommand}' to quit this application at any time.\n");
         }
     }
     #endregion
