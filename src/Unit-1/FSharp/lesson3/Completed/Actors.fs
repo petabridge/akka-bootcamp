a4120332e71a38020fdbabe7e171d114eda19902
Youenn-Bouglouan <youenn.bouglouan@gmail.com>
2017-07-06 21:51:57 +0200

    upgraded to Akka.FSharp 1.2.0, updated code and README accordingly

--- src/Unit-1/FSharp/lesson3/Completed/Actors.fs
+++ src/Unit-1/FSharp/lesson3/Completed/Actors.fs
@@ -1,33 +1,35 @@
-﻿namespace WinTail
+namespace WinTail
 
 open System
 open Akka.Actor
 open Akka.FSharp
 
 module Actors =
-    let consoleReaderActor (validation: IActorRef) (mailbox: Actor<_>) message = 
+
+    let consoleReaderActor (validation: IActorRef) (mailbox: Actor<_>) message =
         let doPrintInstructions () =
             Console.WriteLine "Write whatever you want into the console!"
             Console.WriteLine "Some entries will pass validation, and some won't...\n\n"
             Console.WriteLine "Type 'exit' to quit this application at any time.\n"
-        
+
         let (|Message|Exit|) (str:string) =
-            match str.ToLower() with
+            match str.ToLower () with
             | "exit" -> Exit
             | _ -> Message(str)
-            
-        let getAndValidateInput () = 
-            let line = Console.ReadLine()
-            match line with
-            | Exit -> mailbox.Context.System.Shutdown ()
-            | _ -> validation <! line
 
+        let getAndValidateInput () =
+            let line = Console.ReadLine ()
+            match line with
+            | Exit -> mailbox.Context.System.Terminate () |> ignore
+            | _ -> validation <! line
+            
         match box message with
-        | :? Command as command ->
+        | :? Command as command -> 
             match command with
             | Start -> doPrintInstructions ()
             | _ -> ()
         | _ -> ()
+        
         getAndValidateInput ()
 
 
@@ -42,21 +44,21 @@ module Actors =
         match box message with
         | :? InputResult as inputResult ->
             match inputResult with
-            | InputError (reason,_) -> printInColor ConsoleColor.Red reason
+            | InputError (reason, _) -> printInColor ConsoleColor.Red reason
             | InputSuccess reason -> printInColor ConsoleColor.Green reason
         | _ -> printInColor ConsoleColor.Yellow (message.ToString ())
 
 
-    let validationActor (consoleWriter: IActorRef) (mailbox: Actor<_>) message = 
-        let (|EmptyMessage|MessageLengthIsEven|MessageLengthIsOdd|) (msg:string) = 
+    let validationActor (consoleWriter: IActorRef) (mailbox: Actor<_>) message =
+        let (|EmptyMessage|MessageLengthIsEven|MessageLengthIsOdd|) (msg:string) =
             match msg.Length, msg.Length % 2 with
-            | 0,_ -> EmptyMessage
-            | _,0 -> MessageLengthIsEven
-            | _,_ -> MessageLengthIsOdd
+            | 0, _ -> EmptyMessage
+            | _, 0 -> MessageLengthIsEven
+            | _, _ -> MessageLengthIsOdd
 
         match message with
-        | EmptyMessage ->  consoleWriter <! InputError ("No input received.", ErrorType.Null)
-        | MessageLengthIsEven -> consoleWriter <! InputSuccess ("Thank you! Message was valid.")
-        | _ -> consoleWriter <! InputError ("Invalid: input had odd number of characters.", ErrorType.Validation)
-        mailbox.Sender ()  <! Continue
-               
+        | EmptyMessage -> consoleWriter <! InputError ("No input received.", ErrorType.Null)
+        | MessageLengthIsEven -> consoleWriter <! InputSuccess ("Thank you! The message was valid.")
+        | _ -> consoleWriter <! InputError ("The message is invalid (odd number of characters)!", ErrorType.Validation)
+
+        mailbox.Sender () <! Continue
--- src/Unit-1/FSharp/lesson3/Completed/Program.fs
+++ src/Unit-1/FSharp/lesson3/Completed/Program.fs
@@ -4,11 +4,12 @@ open Akka.Actor
 open WinTail
 
 [<EntryPoint>]
-let main argv = 
+let main argv =
     let myActorSystem = System.create "MyActorSystem" (Configuration.load ())
-    let consoleWriterActor = spawn myActorSystem "consoleWriterActor" (actorOf Actors.consoleWriterActor)
+    let consoleWriterActor = spawn myActorSystem "consoleWriterActor" (actorOf Actors.consoleWriterActor)   
     let validationActor = spawn myActorSystem "validationActor" (actorOf2 (Actors.validationActor consoleWriterActor))
     let consoleReaderActor = spawn myActorSystem "consoleReaderActor" (actorOf2 (Actors.consoleReaderActor validationActor))
+    
     consoleReaderActor <! Start
-    myActorSystem.AwaitTermination ()
+    myActorSystem.WhenTerminated.Wait ()
     0
