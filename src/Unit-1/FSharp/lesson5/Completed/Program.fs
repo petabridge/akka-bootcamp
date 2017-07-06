eebfd2648b3c4632028d33ae17c4c571ae515890
Youenn-Bouglouan <youenn.bouglouan@gmail.com>
2017-07-06 22:26:14 +0200

    upgraded Unit1/Lesson5 to Akka.FSharp 1.2.0, updated code and README accordingly

--- src/Unit-1/FSharp/lesson5/Completed/Program.fs
+++ src/Unit-1/FSharp/lesson5/Completed/Program.fs
@@ -4,19 +4,20 @@ open Akka.Actor
 open WinTail
 
 [<EntryPoint>]
-let main argv = 
-
+let main argv =
+    
     let strategy () = Strategy.OneForOne((fun ex ->
-        match ex with 
-        | :? ArithmeticException  -> Directive.Resume
+        match ex with
+        | :? ArithmeticException -> Directive.Resume
         | :? NotSupportedException -> Directive.Stop
-        | _ -> Directive.Restart), 10, TimeSpan.FromSeconds(30.))
+        | _ -> Directive.Restart), 10, TimeSpan.FromSeconds 30.)
 
     let myActorSystem = System.create "MyActorSystem" (Configuration.load ())
     let consoleWriterActor = spawn myActorSystem "consoleWriterActor" (actorOf Actors.consoleWriterActor)
     let tailCoordinatorActor = spawnOpt myActorSystem "tailCoordinatorActor" (actorOf2 Actors.tailCoordinatorActor) [ SpawnOption.SupervisorStrategy(strategy ()) ]
     let fileValidatorActor = spawn myActorSystem "validationActor" (actorOf2 (Actors.fileValidatorActor consoleWriterActor))
     let consoleReaderActor = spawn myActorSystem "consoleReaderActor" (actorOf2 (Actors.consoleReaderActor))
+    
     consoleReaderActor <! Start
-    myActorSystem.AwaitTermination ()
+    myActorSystem.WhenTerminated.Wait ()
     0
