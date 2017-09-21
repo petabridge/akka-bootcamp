d3ac0cac9f3e891d12e21a81308a8f24749503f4
Youenn-Bouglouan <youenn.bouglouan@gmail.com>
2017-09-21 23:03:56 +0200

    ported Unit 3 / Lesson 3 to F#

---
+++ src/Unit-3/lesson3/Completed/GithubClientFactory.fs
@@ -0,0 +1,16 @@
+﻿namespace GithubActors
+
+module GithubClientFactory =
+
+    open Octokit
+    open Octokit.Internal
+
+    let mutable oauthToken = ""
+
+    let setOauthToken token = oauthToken <- token
+
+    let getUnauthenticatedClient () =
+        GitHubClient (ProductHeaderValue "AkkaBootcamp-Unit3")
+
+    let getClient () =
+         GitHubClient (ProductHeaderValue "AkkaBootcamp-Unit3", InMemoryCredentialStore (Credentials oauthToken))
