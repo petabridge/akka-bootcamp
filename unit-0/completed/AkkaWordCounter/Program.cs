77056b05c1c6945f712e9745a646b09c96c84c37
Aaron Stannard <aaron@petabridge.com>
2025-01-27 15:01:45 -0600

    adding `AkkaWordCounter` sample

---
+++ unit-0/completed/AkkaWordCounter/Program.cs
@@ -0,0 +1,6 @@
+﻿using Akka.Actor;
+using Akka.Event;
+
+ActorSystem myActorSystem = ActorSystem.Create("LocalSystem");
+myActorSystem.Log.Info("Hello from the ActorSystem");
+await myActorSystem.Terminate();
