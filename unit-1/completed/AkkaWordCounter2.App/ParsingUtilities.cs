87cee3c3258592f997f9723cac1b75367568ba9a
Aaron Stannard <aaron@petabridge.com>
2025-02-03 19:58:45 -0600

    Added text extraction

---
+++ unit-1/completed/AkkaWordCounter2.App/ParsingUtilities.cs
@@ -0,0 +1,35 @@
+using HtmlAgilityPack;
+
+namespace AkkaWordCounter2.App;
+
+public static class ParsingUtilities
+{
+    /// <summary>
+    /// Extracts raw text from a HtmlDocument
+    /// </summary>
+    /// <remarks>
+    /// Shouldn't pick up stuff from script / style tags etc
+    /// </remarks>
+    public static IEnumerable<string> ExtractText(HtmlDocument htmlDocument)
+    {
+        var root = htmlDocument.DocumentNode;
+        foreach (var node in root.DescendantsAndSelf())
+        {
+            if (!node.HasChildNodes)
+            {
+                string text = node.InnerText;
+                if (!string.IsNullOrEmpty(text))
+                    yield return text.Trim();
+            }
+        }
+    }
+    
+    public static IEnumerable<string> ExtractTokens(string text)
+    {
+        var tokens = text.Split([' ', '\n', '\r', '\t'], StringSplitOptions.RemoveEmptyEntries);
+        foreach (var token in tokens)
+        {
+            yield return token.Trim();
+        }
+    }
+}
