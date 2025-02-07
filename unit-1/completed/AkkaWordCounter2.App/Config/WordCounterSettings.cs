b49c0b1351f976d65f8f166d8f9c15eda5c89306
Aaron Stannard <aaron@petabridge.com>
2025-02-07 14:58:12 -0600

    cleaning up namespaces

--- unit-1/completed/AkkaWordCounter2.App/Config/WordCounterSettings.cs
+++ unit-1/completed/AkkaWordCounter2.App/Config/WordCounterSettings.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 
 namespace AkkaWordCounter2.App.Config;
